1764c9d16d01f6e04542549ca08bd7655fb1bfb6
毕殿卿 <bdq123@outlook.com>
2020-06-17 23:29:17 +0800

    添加项目文件。

---
+++ ScanQRCodeUploadFile/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using ScanQRCodeUploadFile.Models;
+
+namespace ScanQRCodeUploadFile.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHubContext<NotificationHub> _hubContext;
+        public HomeController(ILogger<HomeController> logger, IHubContext<NotificationHub> hubContext)
+        {
+            _logger = logger;
+            _hubContext = hubContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> Upload(string connectionId)
+        {
+            string imageUrl = "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg";
+
+            await _hubContext.Clients.Client(connectionId).SendAsync("ReceiveMessage", imageUrl);
+
+            return Json(new { success = true, connectionId = connectionId });
+        }
+    }
+}
